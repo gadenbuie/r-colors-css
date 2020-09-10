665eb59e1d2b802abb42a28c38c0ed8090897b94
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2020-09-10 10:21:40 -0400

    init r colors css

---
+++ make-r-colors-css.R
@@ -0,0 +1,24 @@
+library(glue)
+
+r_colors_rgb <- lapply(colors(), function(color) {
+  glue_data(
+    as.data.frame(t(col2rgb(color))),
+    "rgb({red},{green},{blue})"
+  )
+})
+
+r_colors <- data.frame(
+  name = colors(),
+  color = unlist(r_colors_rgb),
+  stringsAsFactors = FALSE
+)
+
+r_colors_css <- c(
+  ":root {",
+  glue_data(r_colors, "  --{name}: {color};", .trim = FALSE),
+  "}",
+  glue_data(r_colors, ".{name} {{ color: var(--{name}); }}"),
+  glue_data(r_colors, ".bg-{name} {{ background-color: var(--{name}); }}")
+)
+
+writeLines(r_colors_css, "r-colors.css")
