37d246e047a154ac34d156fcbac64b782a7160a9
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2020-09-11 10:53:01 -0400

    Modularizes R colors CSS files

--- make-r-colors-css.R
+++ make-r-colors-css.R
@@ -22,12 +22,34 @@ r_colors$hex <- vapply(r_colors$color, hex, character(1))
 
 saveRDS(r_colors, "r_colors.rds")
 
-r_colors_css <- c(
-  ":root {",
-  glue_data(r_colors, "  --{name}: {color};", .trim = FALSE),
-  "}",
-  glue_data(r_colors, ".{name} {{ color: var(--{name}); }}"),
-  glue_data(r_colors, ".bg-{name} {{ background-color: var(--{name}); }}")
+dir.create("dist", showWarnings = FALSE)
+
+glue_colors <- function(...) {
+  glue_data(r_colors, ...)
+}
+
+r_colors_license <- c(
+  "/* R Colors: https://github.com/gadenbuie/r-colors-css */",
+  "/* This is free and unencumbered software released into the public domain by Garrick Aden-Buie */"
 )
 
-writeLines(r_colors_css, "r-colors.css")
+r_colors_vars_css <- c(
+  ":root {",
+  glue_data(r_colors, "  --{name}: {color};", .trim = FALSE),
+  "}"
+)
+r_colors_fg_css <- glue_colors(".{name} {{ color: {color}; }}")
+r_colors_bg_css <- glue_colors(".bg-{name} {{ background-color: {color}; }}")
+r_colors_fg_hover_css <- glue_colors(".{name}-hover:hover {{ color: {color}; }}")
+r_colors_bg_hover_css <- glue_colors(".bg-{name}-hover:hover {{ background-color: {color}; }}")
+
+
+writeLines(
+  c(r_colors_license, r_colors_fg_css, r_colors_bg_css),
+  "dist/r-colors.css"
+)
+writeLines(c(r_colors_license, r_colors_vars_css), "dist/r-colors.vars.css")
+writeLines(
+  c(r_colors_license, r_colors_fg_hover_css, r_colors_bg_hover_css),
+  "dist/r-colors.hover.css"
+)
